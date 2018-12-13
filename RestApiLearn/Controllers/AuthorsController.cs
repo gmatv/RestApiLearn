2139019f9fa897d4244fbdddba87cc614b4ad63f
Gennadiy Matveyenko <gennadiy.matveyenko@gconnect.ch>
2018-12-13 10:01:23 +0200

    Adding entities, database, dto, controller

---
+++ RestApiLearn/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RestApiLearn.Dto;
+using RestApiLearn.Entities;
+using RestApiLearn.Helpers;
+using RestApiLearn.Services;
+
+namespace RestApiLearn.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthorsController
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<AuthorDto> GetAuthors()
+        {
+            var authorDtos = _libraryRepository
+                .GetAuthors()
+                .Select(a =>
+                    new AuthorDto
+                    {
+                        Id = a.Id,
+                        Name = $"{a.FirstName} {a.LastName}",
+                        Genre = a.Genre,
+                        Age = a.DateOfBirth.GetCurrentAge()
+                    })
+                .ToList();
+
+            return authorDtos;
+        }
+    }
+}
---
+++ RestApiLearn/Dto/AuthorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RestApiLearn.Dto
+{
+    public class AuthorDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
